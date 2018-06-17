27fbc151461f8b19f16ec8bb2d21259aa5fa9142
nhoxzunbmt <nhoxzunbmt@users.noreply.github.com>
2018-06-17 22:47:22 +0700

    [U]

--- wp-content/themes/bds/resources/views/index.blade.php
+++ wp-content/themes/bds/resources/views/index.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
-{{--@section('content')--}}
-  {{--@include('partials.page-header')--}}
+@section('content')
+  @include('partials.page-header')
 
-  {{--@if (!have_posts())--}}
-    {{--<div class="alert alert-warning">--}}
-      {{--{{ __('Sorry, no results were found.', 'sage') }}--}}
-    {{--</div>--}}
-    {{--{!! get_search_form(false) !!}--}}
-  {{--@endif--}}
+  @if (!have_posts())
+    <div class="alert alert-warning">
+      {{ __('Sorry, no results were found.', 'sage') }}
+    </div>
+    {!! get_search_form(false) !!}
+  @endif
 
-  {{--@while (have_posts()) @php the_post() @endphp--}}
-    {{--{{ 'partials.content-'.get_post_type() }}--}}
-    {{--@include('partials.content-'.get_post_type())--}}
-  {{--@endwhile--}}
+  @while (have_posts()) @php the_post() @endphp
+  {{ 'partials.content-'.get_post_type() }}
+  @include('partials.content-'.get_post_type())
+  @endwhile
 
-  {{--{!! get_the_posts_navigation() !!}--}}
-{{--@endsection--}}
+  {!! get_the_posts_navigation() !!}
+@endsection
